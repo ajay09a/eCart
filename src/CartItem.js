c2f918fa829a23706e4b5597d3246ba15ed99baa
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-02-28 18:25:48 +0530

    third commit

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,33 @@
 import React  from "react";
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            Qty: 1,
+            img: ''
+        }
+        this.increaseQuantity = () => {
+            console.log('this', this.state);
+        }
+    }
     render(){
+        const {title, price, Qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25}}>Phone</div>
-                    <div style={ {color: '#777'}}>Rs. 999</div>
-                    <div style={ {color: '#777'}}>Qty: 1</div>
+                    <div style={ {fontSize: 25}}>{title}</div>
+                    <div style={ {color: '#777'}}>{price}</div>
+                    <div style={ {color: '#777'}}>{Qty}</div>
                     <div className="cart-item-actions">
-                        {/* buttons */}
+                        <img alt="increase" className="action-icons" onClick={this.increaseQuantity} src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
+                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                        <img alt="delete" className="action-icons" src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" />
                     </div>
                 </div>
             </div>
