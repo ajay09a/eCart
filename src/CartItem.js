388b692ef682f96f409429640811dfa33429da18
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-02-28 19:03:31 +0530

    react state

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,7 +10,30 @@ class CartItem extends React.Component{
             img: ''
         }
         this.increaseQuantity = () => {
-            console.log('this', this.state);
+            // console.log('this', this.state);
+
+            // form state 1
+            // this.setState({
+            //     Qty: this.state.Qty + 1
+            // })
+
+            // form state 2
+            this.setState((prevState)=>{
+                return {
+                    Qty: prevState.Qty + 1
+                }
+            })
+        }
+        this.decreaseQuantity = () => {
+            const {Qty} = this.state;
+            if(Qty===0){
+                return;
+            }
+            this.setState((prevState)=>{
+                return {
+                    Qty: prevState.Qty - 1
+                }
+            })
         }
     }
     render(){
@@ -26,7 +49,7 @@ class CartItem extends React.Component{
                     <div style={ {color: '#777'}}>{Qty}</div>
                     <div className="cart-item-actions">
                         <img alt="increase" className="action-icons" onClick={this.increaseQuantity} src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
-                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                        <img alt="decrease" className="action-icons" onClick={this.decreaseQuantity} src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
                         <img alt="delete" className="action-icons" src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" />
                     </div>
                 </div>
