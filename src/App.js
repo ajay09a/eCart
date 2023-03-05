fa6c205071bfd7f5e77546d6f18caa5d595e5e3e
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-03-05 15:49:55 +0530

    total amount paid and product images

--- src/App.js
+++ src/App.js
@@ -11,21 +11,21 @@ class App extends React.Component {
                     price: 99,
                     title: 'Watch',
                     Qty: 1,
-                    img: '',
+                    img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d3Jpc3QlMjB3YXRjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
                     id: 1
                 },
                 {
                     price: 999,
                     title: 'Phone',
                     Qty: 2,
-                    img: '',
+                    img: 'https://images.unsplash.com/photo-1575695342320-d2d2d2f9b73f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnQlMjBwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
                     id: 2
                 },
                 {
                     price: 9999,
                     title: 'Laptop',
                     Qty: 4,
-                    img: '',
+                    img: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
                     id: 3
                 }
             ]
@@ -65,6 +65,14 @@ class App extends React.Component {
       });
       return count;
     }
+    getTotalAmount = ()=>{
+      const {products} = this.state;
+      let count = 0;
+      products.map((product) => {
+        count += product.Qty * product.price;
+      });
+      return count;
+    }
   render(){
     const {products} = this.state;
     return (
@@ -76,6 +84,7 @@ class App extends React.Component {
           ondecreaseQuantity = {this.handledecreaseQuantity}
           onDeleteProduct = {this.handleDeleteProduct}
         />
+        <div style={{padding:10, fontSize:20}}>Total: {this.getTotalAmount()}</div>
       </div>
     );
   }
