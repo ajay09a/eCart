4571ecbdd559fb95e693c7ab7effa04234d257dc
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-03-05 15:08:51 +0530

    adding navbar

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,6 @@
 import React  from "react";
 
-class CartItem extends React.Component{
+const CartItem = (props)=>{
     
             
     //     this.increaseQuantity = () => {
@@ -30,28 +30,26 @@ class CartItem extends React.Component{
     //         })
     //     }
     // }
-    render(){
-        console.log("this.props", this.props);
-        const {title, price, Qty} = this.props.product;
-        const {product, onIncreaseQuantity, ondecreaseQuantity, onDeleteProduct} = this.props;
-        return(
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image}/>
-                </div>
-                <div className="right-block">
-                    <div style={ {fontSize: 25}}>{title}</div>
-                    <div style={ {color: '#777'}}>{price}</div>
-                    <div style={ {color: '#777'}}>{Qty}</div>
-                    <div className="cart-item-actions">
-                        <img alt="increase" className="action-icons" onClick={()=>this.props.onIncreaseQuantity(this.props.product)} src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
-                        <img alt="decrease" className="action-icons" onClick={()=>this.props.ondecreaseQuantity(this.props.product)} src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
-                        <img alt="delete" className="action-icons" onClick={()=>onDeleteProduct(product.id)} src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" />
-                    </div>
+    console.log("this.props", props);
+    const {title, price, Qty} = props.product;
+    const {product, onIncreaseQuantity, ondecreaseQuantity, onDeleteProduct} = props;
+    return(
+        <div className="cart-item">
+            <div className="left-block">
+                <img style={styles.image}/>
+            </div>
+            <div className="right-block">
+                <div style={ {fontSize: 25}}>{title}</div>
+                <div style={ {color: '#777'}}>{price}</div>
+                <div style={ {color: '#777'}}>{Qty}</div>
+                <div className="cart-item-actions">
+                    <img alt="increase" className="action-icons" onClick={()=>props.onIncreaseQuantity(props.product)} src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
+                    <img alt="decrease" className="action-icons" onClick={()=>props.ondecreaseQuantity(props.product)} src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                    <img alt="delete" className="action-icons" onClick={()=>onDeleteProduct(product.id)} src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const styles = {
